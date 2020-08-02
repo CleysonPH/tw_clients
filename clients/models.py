af0ab8c186472886c437e12875cfc952dabdd6e3
Cleyson Lima <cleysonph@gmail.com>
2020-08-02 01:39:36 -0300

    Created the model Client and the migrations for that model

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    GENDER_CHOICES = (
+        ("F", "Feminino"),
+        ("M", "Masculino"),
+        ("N", "Nenhuma das opções"),
+    )
+
+    name = models.CharField("Nome", max_length=100, null=False, blank=False)
+    birth_date = models.DateField("Data de Nascimento", null=False, blank=False)
+    email = models.EmailField("Email", null=False, blank=False)
+    occupation = models.CharField("Profissão", max_length=50, null=False, blank=False)
+    gender = models.CharField(
+        "Sexo", max_length=1, choices=GENDER_CHOICES, blank=False, null=False
+    )
+
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clientes"
+
+    def __str__(self):
+        return self.name
